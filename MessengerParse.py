8b813ee1eedc6028ca140e663a17a37bf6dfd2ab
Beers <adbeers@wpi.edu>
2017-12-13 12:21:29 -0500

    First Pass of parsing mssg data, need emojies

---
+++ MessengerParse.py
@@ -0,0 +1,45 @@
+#Script which parses a facebook data file after being copied into a .txt file.
+#The script will count the frequency of each word in the data and save results to a file.
+#Author: August Beers
+
+import string
+import re
+
+TokFound = []
+TokCount = []
+
+with open("CharMessengerData.txt") as f:
+
+    header_ignore_flip = False
+
+    for line in f:
+        no_new_line = line[:-1]
+
+        regex = re.compile('[%s]' % re.escape(string.punctuation))
+
+        punc_strip_line = regex.sub('', no_new_line)
+
+        #print(no_new_line)
+        #print(punc_strip_line)
+
+        tokens = punc_strip_line.split(' ')
+
+
+        if(len(punc_strip_line) == 0):
+            header_ignore_flip = True
+        elif header_ignore_flip == True:
+            header_ignore_flip = False
+        else:
+            for e in tokens:
+
+                if e in TokFound:
+                    i = TokFound.index(e)
+                    TokCount[i] += 1
+                else:
+                    TokFound.append(e)
+                    TokCount.append(1)
+
+finalTokData = zip(TokFound,TokCount)
+soretedFinalTokData = sorted(finalTokData, key = lambda x:x[1], reverse = True)
+for e in list(soretedFinalTokData):
+    print(e)
